6588f6c8701730f33e37fb25417a94907bee333d
claire <github email address>
2018-12-19 11:24:48 -0600

    Completed Minimum

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,29 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+
+    def create
+        @employee = Employee.create(employee_params)
+        redirect_to @employee
+    end
+
+    private
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id)
+    end
+
+
+
+
 end
