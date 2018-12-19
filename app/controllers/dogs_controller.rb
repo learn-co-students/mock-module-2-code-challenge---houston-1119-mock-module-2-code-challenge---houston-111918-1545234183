1942607b82ea9a727594676b09bdacd5af88de9c
github username <github email address>
2018-12-19 13:08:26 -0600

    2nd mock-code-challenge

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,40 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all
+    end
+    
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+    def new
+        @dog = Dog.new
+    end
+
+    def create
+        Dog.create(dog_params)
+        redirect_to dogs_path
+    end
+    
+    def edit
+        @dog = Dog.find(params[:id])
+    end
+
+    def update
+        @dog = Dog.find(params[:id])
+        dog.update(dog_params)
+        redirect_to dog
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def dog_params
+        params.require(:dog).permit(:name, :breed, :age)
+    end
+
 end
