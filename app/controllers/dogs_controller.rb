4c940ec801fb8a55719427295c34334d3f7c7fb5
github username <github email address>
2018-12-19 11:27:24 -0600

    done 1

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,21 @@
 class DogsController < ApplicationController
+
+  def index
+    @dog = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  def create
+    Dog.create(dog_params)
+    redirect_to dogs_path
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age)
+  end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,23 @@
 class EmployeesController < ApplicationController
+  def index
+    @employee = Employee.all
+  end
+    
+  def show
+    @employee = Employee.find(params[:id])
+  end
+    
+  def create
+    Employee.create(employee_params)
+    redirect_to employees_path
+  end
+
+  def new
+  end
+    
+private
+    
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id)
+  end
 end
