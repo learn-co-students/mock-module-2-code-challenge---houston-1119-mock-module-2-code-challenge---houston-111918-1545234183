0bcee39aaf9f4f98a4b40d70916f100c065e129b
claire <github email address>
2018-12-19 12:04:59 -0600

    cleaned up code, added edit/update functionalities

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -13,14 +13,25 @@ class EmployeesController < ApplicationController
     end
 
     def create
-        @employee = Employee.create(employee_params)
-        redirect_to @employee
+        employee = Employee.create(employee_params)
+        redirect_to employee
     end
+
+    def edit
+        @employee = Employee.find(params[:id])
+    end
+
+    def update
+        employee = Employee.find(params[:id])
+        employee.update(employee_params)
+        redirect_to employee
+    end
+
 
     private
 
     def employee_params
-        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id)
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
     end
 
 
