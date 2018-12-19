ea8128cfe89c583a811516e836b76dfe84c28d0c
rdaley321 <rdaley321@yahoo.com>
2018-12-19 10:50:15 -0600

    minimum bar v2

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -5,4 +5,16 @@ class DogsController < ApplicationController
   def show
     @dog = Dog.find(params[:id])
   end
+  def new
+    @dog = Dog.new
+  end
+  def create
+    dog = Dog.create(dog_params)
+    redirect_to dog_path(dog)
+  end
+
+  private
+  def dog_params
+    params.require(:dog).permit(:name,:age,:breed)
+  end
 end
